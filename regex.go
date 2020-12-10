3a34cb5eb437d34423de32cfaf48e860664ed887
Noah Harris <dakoolstwunn@gmail.com>
2020-12-09 19:48:00 -0500

    Add regex patterns for nick and email

---
+++ regex.go
@@ -0,0 +1,15 @@
+package monkelib
+
+import (
+	"regexp"
+)
+
+const (
+	NICK_PATTERN  = `[A-Za-z0-9.\-_]{3,16}`
+	EMAIL_PATTERN = `[^@]+@[^@]+`
+)
+
+var (
+	NickRegex  *regexp.Regexp = regexp.MustCompile("^" + NICK_PATTERN + "$")
+	EmailRegex *regexp.Regexp = regexp.MustCompile("^" + EMAIL_PATTERN + "$")
+)
---
+++ regex_test.go
@@ -0,0 +1,52 @@
+package monkelib
+
+import (
+	"testing"
+)
+
+type regexCase struct {
+	Condition string
+	Pass      bool
+}
+
+func Test_NickRegex(test *testing.T) {
+	var cases []regexCase = []regexCase{
+		regexCase{"gastrodon", true},
+		regexCase{"jim", true},
+		regexCase{"jumbounclesteve", true},
+		regexCase{"___lllIIIlll___", true},
+		regexCase{"zero", true},
+		regexCase{"...", true},
+		regexCase{"foobar.io", true},
+		regexCase{"", false},
+		regexCase{"foo bar", false},
+		regexCase{"|hello|", false},
+		regexCase{"thisnameiswaytoolongwhoallowedthis", false},
+		regexCase{"foo@bar.io", false},
+	}
+
+	var testcase regexCase
+	for _, testcase = range cases {
+		if NickRegex.MatchString(testcase.Condition) != testcase.Pass {
+			test.Errorf("%s -> %t", testcase.Condition, testcase.Pass)
+		}
+	}
+}
+
+func Test_EmailRegex(test *testing.T) {
+	var cases []regexCase = []regexCase{
+		regexCase{"gastrodon", false},
+		regexCase{"gastrodon@", false},
+		regexCase{"@gastrodon", false},
+		regexCase{"@", false},
+		regexCase{"@@@", false},
+		regexCase{"gastrodon@localhost", true},
+	}
+
+	var testcase regexCase
+	for _, testcase = range cases {
+		if EmailRegex.MatchString(testcase.Condition) != testcase.Pass {
+			test.Errorf("%s -> %t", testcase.Condition, testcase.Pass)
+		}
+	}
+}
